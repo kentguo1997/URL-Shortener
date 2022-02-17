fab572f2d56444227394f85294b5ac8e9a6d06e3
kentguo_1997 <guoyunkai201709@gmail.com>
2022-02-17 18:33:49 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 // Include packages in the project
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 const port = 3000
 
 const app = express()
+
+
+// setting express-handlebars as template engine
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 
 // connect to mongodb
 mongoose.connect('mongodb://localhost/URL-shortener') 
@@ -21,7 +28,7 @@ db.once ('open', () => {
 
 // routes setting
 app.get('/', (req, res) => {
-  res.send('This is the project for URL-shortener')
+  res.render('index')
 })
 
 
